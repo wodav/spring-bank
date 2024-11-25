b07df24fa45292d647c34177a3aa4a77f1fadf5b
wodav <wolff_david@outlook.com>
2024-11-25 20:30:58 +0100

    feature/code-generation: Write test for User endpoint create

--- src/test/java/spring/bank/BankApplicationTests.java
+++ src/test/java/spring/bank/BankApplicationTests.java
@@ -1,13 +1,35 @@
 package spring.bank;
 
+import net.minidev.json.JSONObject;
 import org.junit.jupiter.api.Test;
+import org.openapitools.dto.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment=SpringBootTest.WebEnvironment.RANDOM_PORT)
 class BankApplicationTests {
-
+	@Autowired
+	protected TestRestTemplate testRestTemplate;
+	protected ResponseEntity response;
 	@Test
 	void contextLoads() {
+		HttpHeaders headers = new HttpHeaders();
+		JSONObject newUser = new JSONObject();
+
+		//headers.add("Authorization","Bearer "+ jwtToken);
+		headers.add("Content-Type","application/json");
+		newUser.put("username","Ben");
+
+		HttpEntity httpEntity = new HttpEntity<>(newUser,headers);
+
+		response=testRestTemplate.exchange("/users", HttpMethod.POST,httpEntity, User.class);
+		System.out.print("Hhaha");
 	}
 
 }
