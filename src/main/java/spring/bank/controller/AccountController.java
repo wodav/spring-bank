a8ccfd57571bc227b5e3bf0988d2ac8a0a4729d3
wodav <wolff_david@outlook.com>
2024-11-29 15:06:22 +0100

    feature/delete-account: Implement delete of account
    * controller
    * service

--- src/main/java/spring/bank/controller/AccountController.java
+++ src/main/java/spring/bank/controller/AccountController.java
@@ -2,6 +2,7 @@ package spring.bank.controller;
 
 import org.openapitools.api.AccountsApi;
 import org.openapitools.dto.AccountDto;
+import org.openapitools.dto.UserDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,5 +21,15 @@ public class AccountController implements AccountsApi {
     @Override
     public ResponseEntity<List<AccountDto>> accountsGet() {
         return AccountsApi.super.accountsGet();
+    }
+
+    @Override
+    public ResponseEntity<AccountDto> deleteAccount(Integer id) {
+        try{
+            AccountDto accountDto = accountService.delete(id);
+            return new ResponseEntity<>(accountDto,HttpStatus.OK);
+        } catch (NullPointerException e){
+            return new ResponseEntity<>(null,HttpStatus.NO_CONTENT);
+        }
     }
 }
--- src/main/java/spring/bank/services/AccountService.java
+++ src/main/java/spring/bank/services/AccountService.java
@@ -1,16 +1,39 @@
 package spring.bank.services;
 
+import org.modelmapper.ModelMapper;
 import org.openapitools.dto.AccountDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import spring.bank.entities.Account;
+import spring.bank.repositories.AccountRepository;
+import spring.bank.repositories.UserRepository;
 
 import java.io.IOException;
+import java.util.Optional;
 
 @Service
 public class AccountService {
 
-    @Transactional
-    public AccountDto create(AccountDto accountDto) throws IOException {
-        return null;
+    @Autowired
+    AccountRepository accountRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    ModelMapper modelMapper;
+
+    public AccountDto delete(Integer id) {
+
+        Optional<Account> optionalAccount = accountRepository.findById(id.longValue());
+
+        if(optionalAccount.isEmpty()){
+            throw new NullPointerException("Account with id " + id + " not found");
+        }
+        else {
+            Account account = optionalAccount.get();
+            accountRepository.delete(account);
+            return modelMapper.map(account, AccountDto.class);
+        }
     }
 }
