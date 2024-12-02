f4b2907a4316886b26a158e1ca6192486a1e0f4b
wodav <wolff_david@outlook.com>
2024-12-02 16:50:24 +0100

    feature/get-transaction: Get all transactions
    * controller
    * service

--- src/main/java/spring/bank/controller/AccountController.java
+++ src/main/java/spring/bank/controller/AccountController.java
@@ -2,14 +2,12 @@ package spring.bank.controller;
 
 import org.openapitools.api.AccountsApi;
 import org.openapitools.dto.AccountDto;
-import org.openapitools.dto.UserDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import spring.bank.services.AccountService;
 
-import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -20,7 +18,7 @@ public class AccountController implements AccountsApi {
 
     @Override
     public ResponseEntity<List<AccountDto>> accountsGet() {
-        return AccountsApi.super.accountsGet();
+        return new ResponseEntity<>(accountService.getAll(),HttpStatus.OK);
     }
 
     @Override
--- src/main/java/spring/bank/services/AccountService.java
+++ src/main/java/spring/bank/services/AccountService.java
@@ -4,11 +4,13 @@ import org.modelmapper.ModelMapper;
 import org.openapitools.dto.AccountDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import spring.bank.entities.Account;
 import spring.bank.repositories.AccountRepository;
 import spring.bank.repositories.UserRepository;
 
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -35,5 +37,19 @@ public class AccountService {
             accountRepository.delete(account);
             return modelMapper.map(account, AccountDto.class);
         }
+    }
+
+    @Transactional
+    public List<AccountDto> getAll() {
+
+        List<Account> accounts = accountRepository.findAll();
+        List<AccountDto> accountDtoList = new ArrayList<>();
+
+        for(Account account : accounts){
+            AccountDto accountDto = modelMapper.map(account, AccountDto.class);
+            accountDtoList.add(accountDto);
+        }
+
+        return accountDtoList;
     }
 }
