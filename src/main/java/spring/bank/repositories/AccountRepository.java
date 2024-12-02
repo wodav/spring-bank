3f1ad22fde4856e9912d0d8991c1c79c6c9b54fa
wodav <wolff_david@outlook.com>
2024-12-02 19:16:55 +0100

    feature/create-transaction: Build iban

--- src/main/java/spring/bank/repositories/AccountRepository.java
+++ src/main/java/spring/bank/repositories/AccountRepository.java
@@ -1,9 +1,15 @@
 package spring.bank.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import spring.bank.entities.Account;
 
+import java.util.Optional;
+
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    //@Query(value = "SELECT MAX(account_number) FROM Account", nativeQuery = true)
+    Optional<Account> findTopByOrderByAccountNumberDesc();
 }
--- src/main/java/spring/bank/services/AccountService.java
+++ src/main/java/spring/bank/services/AccountService.java
@@ -3,6 +3,7 @@ package spring.bank.services;
 import org.modelmapper.ModelMapper;
 import org.openapitools.dto.AccountDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import spring.bank.entities.Account;
@@ -24,6 +25,15 @@ public class AccountService {
 
     @Autowired
     ModelMapper modelMapper;
+
+    @Value("${iban.countrycode}")
+    String ibanCountryCode;
+
+    @Value("${iban.checkdigits}")
+    String ibanCheckDigits;
+
+    @Value("${iban.bankcode}")
+    String ibanBankCode;
 
     public AccountDto delete(Integer id) {
 
@@ -51,5 +61,28 @@ public class AccountService {
         }
 
         return accountDtoList;
+    }
+
+    public String buildIban(Integer accountNumber) {
+
+        String formattedAccountNumber = String.format("%06d", accountNumber);
+
+        return ibanCountryCode
+                + ibanCheckDigits
+                + ibanBankCode
+                + formattedAccountNumber;
+    }
+
+    //TODO: Safe highest iban in extra value. If account gets deleted account number will given away again!
+    public Integer getHighestAccountNumber() {
+
+        Optional<Account> topAccountNumberOptional = accountRepository.findTopByOrderByAccountNumberDesc();
+
+        if(topAccountNumberOptional.isEmpty()){
+            return  0;
+        }
+        else {
+            return topAccountNumberOptional.get().getAccountNumber();
+        }
     }
 }
